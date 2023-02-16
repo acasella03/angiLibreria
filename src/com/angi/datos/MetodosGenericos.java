c7f9be7e19ea2daaae881c0dd3f15568f165d8bf
Angi Marliu Casella <acasella@danielcastelao.org>
2023-02-16 08:43:16 +0100

    Nueva Clase MetodosGenericos

--- src/com/angi/datos/MetodosGenericos.java
+++ src/com/angi/datos/MetodosGenericos.java
@@ -1,6 +1,7 @@
 package com.angi.datos;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 
 public class MetodosGenericos {
@@ -21,7 +22,7 @@ public class MetodosGenericos {
      */
     public static <T> void mostrar(ArrayList<T>lista){
         for (T ele:lista)
-            System.out.println(ele);
+            System.out.println("Los elemento de la lista son: "+ele);
     }
     
     /**
@@ -34,5 +35,52 @@ public class MetodosGenericos {
         for(T ele:lista){
             System.out.println(ele);
         }
+    }
+    
+    /**
+     * Imprime el elemento de valor más bajo de un ArrayList
+     * @param <T> tipo genérico que será definido una vez seleccionado el método
+     * @param lista donde buscará el elemento
+     */
+    public static <T> void minimoLista(ArrayList<T>lista){
+        System.out.println("El elemento con su valor mínimo es: "+Collections.min(lista,null));
+    }
+    
+    /**
+     * Imprime el elemento de valor más alto de un ArrayList
+     * @param <T> tipo genérico que será definido una vez seleccionado el método
+     * @param lista donde buscará el elemento
+     */
+    public static <T> void maximoLista(ArrayList<T>lista){
+        System.out.println("El elemento con su valor máximo es: "+Collections.max(lista,null));
+    }
+    
+    /**
+     * Busca e imprime un elemento de la lista
+     * @param <T> tipo genérico que será definido una vez seleccionado el método
+     * @param elemento a buscar en la lista
+     * @param lista donde buscará el elemento
+     */
+    public static <T> void buscar(T elemento,ArrayList<T>lista){
+        if(lista.indexOf(elemento)>=0){
+            System.out.println("El elemento "+elemento+" está en la lista");
+        }else{
+            System.out.println("El elemento "+elemento+ "no está en la lista");
+        }
+    }
+    
+    /**
+     * Busca un elemento de la lista y lo elimina
+     * @param <T> tipo genérico que será definido una vez seleccionado el método
+     * @param elemento a buscar y eliminar en la lista
+     * @param lista donde buscará el elemento
+     */
+    public static <T> void eliminar(T elemento,ArrayList<T>lista){
+        if(lista.indexOf(elemento)>=0){
+            lista.remove(elemento);
+        }else{
+            System.out.println("El elemento "+elemento+ "no está en la lista");
+        }
+        
     }
 }
