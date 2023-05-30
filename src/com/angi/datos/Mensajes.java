dc5a1e8e55d820632ba8d9eb3214cbc2f3cfca8f
acasella03 <114586433+acasella03@users.noreply.github.com>
2023-05-30 22:17:58 +0200

    Nueva clase Mensajes

---
+++ src/com/angi/datos/Mensajes.java
@@ -0,0 +1,9 @@
+package com.angi.datos;
+
+import javax.swing.JOptionPane;
+
+public class Mensajes {
+    public static void mostrarMensaje(String mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+}
